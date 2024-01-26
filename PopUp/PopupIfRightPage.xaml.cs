09e149d6f81dc14a75b0dd8465e40e23bdfac28f
Dmitr <141524559+Dmitri-8799@users.noreply.github.com>
2024-01-26 20:32:54 +0300

    Add files via upload

---
+++ PopUp/PopupIfRightPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace SocialSciencesEF2024.PopUp;
+
+public partial class PopupIfRightPage : Popup
+{
+	public PopupIfRightPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PopUp/PopupIfWrongPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace SocialSciencesEF2024.PopUp;
+
+public partial class PopupIfWrongPage : Popup
+{
+	public PopupIfWrongPage()
+	{
+		InitializeComponent();
+	}
+}
