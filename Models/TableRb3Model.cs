8db0d5d24398cda4f4e30b759d84c20238f27c2e
Dmitr <141524559+Dmitri-8799@users.noreply.github.com>
2024-01-27 11:47:38 +0300

    Update TableRb3Model.cs

--- Models/TableRb3Model.cs
+++ Models/TableRb3Model.cs
@@ -7,72 +7,79 @@ namespace SocialSciencesEF2024.Models
 	{
         //я перенес все свойства с таблицы сюда, реализовал INotifyPropertyChanged, но у меня вопрос - нам надо в модели создавать поля для получения value у кадждого определенного в модели свойства ?
 
+//нам надо как-то устанавливать свойства id? Указывать первичный ключ и тд с помощью атрибута / аннотации ?
         public int Id { get; set; }
 
        
-
-        public string? Option1
+	public string? option1;
+        public string Option1
         {
-            get { return Option1; }
+            get { return option1; }
             set
             {
-                _ = value;
+                option1 = value;
                 OnPropertyChanged("Option1");
             }
         }
-
-        public string Option2
+	
+	public string? option2;
+	public string Option2
         {
-            get { return Option2; }
+            get { return option2; }
             set
             {
 
-                _ = value;
+                option2 = value;
                 OnPropertyChanged("Option2");
             }
 
         }
+	public string? option3;
         public string Option3
         {
-            get { return Option3; }
+            get { return option3; }
             set
             {
 
-                _ = value;
+                option3 = value;
                 OnPropertyChanged("Option3");
             }
         }
 
+	private int? answerNr;
         public int AnswerNr
         {
-            get { return AnswerNr; }
+            get { return (int)answerNr; }
             set
             {
 
-                _ = value;
+                answerNr = value;
                 OnPropertyChanged("AnswerNr");
             }
         }
 
-        public string IfRight
+        
+	public string ifRight;
+ 	public string IfRight
         {
-            get { return IfRight; }
+            get { return ifRight; }
             set
             {
 
-                _ = value;
-                OnPropertyChanged("_ifRight");
+                ifRight = value;
+                OnPropertyChanged("IfRight");
             }
         }
 
-        public string IfWrong
+	public string ifWrong;        
+	public string IfWrong
         {
-            get { return IfWrong; }
+            get { return ifWrong; }
             set
             {
 
-                _ = value;
-                OnPropertyChanged("_ifWrong");
+                ifWrong = value;
+                OnPropertyChanged("IfWrong");
             }
         }
 
